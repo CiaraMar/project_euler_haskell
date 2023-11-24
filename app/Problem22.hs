f432d6029c1b737352b5788b9d280ee78f74fc69
Maeve <Maeve>
2023-11-24 07:03:54 -0800

    Adding Problem22

--- app/Problem22.hs
+++ app/Problem22.hs
@@ -1,4 +1,18 @@
 module Problem22 where
 
-problem22 :: Int
-problem22 = undefined
+import qualified Data.ByteString as B (ByteString, filter, split, unpack)
+import Data.List (sort)
+import Types (ProblemWrapper)
+import Utils (wrapProblem)
+
+parse :: B.ByteString -> [B.ByteString]
+parse = B.split 44 . B.filter (/= 34)
+
+wordToInts :: B.ByteString -> [Int]
+wordToInts = map (fromIntegral . subtract 64) . B.unpack
+
+_problem22 :: [B.ByteString] -> Int
+_problem22 = sum . zipWith (*) [1 ..] . map sum . sort . map wordToInts
+
+problem22 :: ProblemWrapper
+problem22 = wrapProblem (_problem22 . parse)
